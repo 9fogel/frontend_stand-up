7bb23f16142833fe72c971c99d818dc1ecd7f05f
Vera Kirayonak <Vera_Kirayonak@epam.com>
2024-02-07 12:56:11 +0300

    feat: add TomSelect plugin for selecting comedian and time

--- main.js
+++ main.js
@@ -1,1 +1,68 @@
-import './style.css'
+import './style.css';
+import TomSelect from 'tom-select';
+
+const bookingComedianList = document.querySelector('.booking__comedians-list');
+
+const createComedianBlock = () => {
+  const bookingComedian = document.createElement('li');
+  bookingComedian.classList.add('booking__comedian');
+
+  const bookingSelectComedian = document.createElement('select');
+  bookingSelectComedian.classList.add('booking__select', 'booking__select_comedian');
+
+  const bookingSelectTime = document.createElement('select');
+  bookingSelectTime.classList.add('booking__select', 'booking__select_time');
+
+  const inputHidden = document.createElement('input');;
+  inputHidden.type = 'hidden';
+  inputHidden.name = 'booking';
+
+  const bookingHall = document.createElement('button');
+  bookingHall.classList.add('booking__hall');
+
+  bookingComedian.append(bookingSelectComedian, bookingSelectTime, inputHidden);
+
+  const bookingTomSelectComedian = new TomSelect(bookingSelectComedian, {
+    hideSelected: true,
+    placeholder: 'Выбрать комика',
+    options: [{
+      value: 1,
+      text: 'Белый',
+    },
+    {
+      value: 2,
+      text: 'Серый',
+    }],
+  });
+
+  const bookingTomSelectTime = new TomSelect(bookingSelectTime, {
+    hideSelected: true,
+    placeholder: 'Время',
+  });
+  bookingTomSelectTime.disable();
+
+  return bookingComedian;
+};
+
+const init = () => {
+
+  const comedianBlock = createComedianBlock();
+  bookingComedianList.append(comedianBlock);
+};
+
+init();
+
+/**
+<li class="booking__comedian">
+  <select class="booking__select booking__select_comedian" name="comedian" >
+    <option value="1">Юлия Ахмедова</option>
+    <option value="2">Слава Комиссаренко</option>
+  </select>
+
+  <select class="booking__select booking__select_time" name="time">
+    <option value="20:00">20:00</option>
+    <option value="22:00">22:00</option>
+  </select>
+
+  <button class="booking__hall">Зал 1</button>
+</li> */
