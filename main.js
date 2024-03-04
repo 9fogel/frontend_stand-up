7df3d23f8c9f8c5fbcf2fa3919776221f372528d
Vera Kirayonak <Vera_Kirayonak@epam.com>
2024-03-04 12:44:40 +0300

    refactor: allocate main.js functions to separate files

--- main.js
+++ main.js
@@ -1,199 +1,27 @@
-import { Notification } from './scripts/notification';
 import './style.css';
-import TomSelect from 'tom-select';
-import Inputmask from 'inputmask';
-import JustValidate from 'just-validate';
-
-const MAX_COMEDIANS = 6;
-
-const notification = Notification.getInstance();
-
-const bookingComedianList = document.querySelector('.booking__comedians-list');
-const bookingForm = document.querySelector('.booking__form');
-
-const createComedianBlock = (comedians) => {
-  const bookingComedian = document.createElement('li');
-  bookingComedian.classList.add('booking__comedian');
-
-  const bookingSelectComedian = document.createElement('select');
-  bookingSelectComedian.classList.add('booking__select', 'booking__select_comedian');
-
-  const bookingSelectTime = document.createElement('select');
-  bookingSelectTime.classList.add('booking__select', 'booking__select_time');
-
-  const inputHidden = document.createElement('input');;
-  inputHidden.type = 'hidden';
-  inputHidden.name = 'booking';
-
-  const bookingHall = document.createElement('button');
-  bookingHall.classList.add('booking__hall');
-  bookingHall.type = 'button';
-
-  bookingComedian.append(bookingSelectComedian, bookingSelectTime, inputHidden);
-
-  const bookingTomSelectComedian = new TomSelect(bookingSelectComedian, {
-    hideSelected: true,
-    placeholder: 'Выбрать комика',
-    options: comedians.map((item) => ({
-      value: item.id,
-      text: item.comedian,
-    })),
-  });
-
-
-  const bookingTomSelectTime = new TomSelect(bookingSelectTime, {
-    hideSelected: true,
-    placeholder: 'Время',
-  });
-  bookingTomSelectTime.disable();
-
-  bookingTomSelectComedian.on('change', (id) => {
-    bookingTomSelectTime.enable();
-    bookingTomSelectComedian.blur();
-
-    const { performances } = comedians.find((item) => item.id === id);
-    bookingTomSelectTime.clear();
-    bookingTomSelectTime.clearOptions();
-    bookingTomSelectTime.addOptions(performances.map((item) => ({
-      value: item.time,
-      text: item.time,
-    })));
-
-    bookingHall.remove();
-  });
-
-  bookingTomSelectTime.on('change', (time) => {
-    if(!time) {
-      return;
-    }
-
-    const idComedian = bookingTomSelectComedian.getValue();
-    const { performances } = comedians.find((item) => item.id === idComedian);
-    const { hall } = performances.find((item) => item.time === time);
-
-    inputHidden.value = `${idComedian},${time}`;
-
-    bookingTomSelectTime.blur();
-    bookingHall.textContent = hall;
-    bookingComedian.append(bookingHall);
-  });
-
-
-  const createNextBookingComedian = () => {
-    if (bookingComedianList.children.length < MAX_COMEDIANS) {
-      const nextComediansBlock = createComedianBlock(comedians);
-      bookingComedianList.append(nextComediansBlock);
-    };
-
-    bookingTomSelectTime.off('change', createNextBookingComedian);
-  }
-
-  bookingTomSelectTime.on('change', createNextBookingComedian);
-
-  return bookingComedian;
-};
-
-
-const getComedians = async () => {
-  const response = await fetch('http://localhost:8080/comedians');
-  return response.json();
-};
-
+import { initForm } from './scripts/form';
+import { getComedians } from './scripts/api';
+import { createComedianBlock } from './scripts/comedians';
 
 const init = async () => {
+  const bookingComedianList = document.querySelector('.booking__comedians-list');
+  const bookingForm = document.querySelector('.booking__form');
+
   const countComedians = document.querySelector('.event__info-item_comedians .event__info-number');
-  const comedians = await getComedians();
-
-  countComedians.textContent = comedians.length;
-
-  const comedianBlock = createComedianBlock(comedians);
-  bookingComedianList.append(comedianBlock);
-
-  const validate = new JustValidate(bookingForm, {
-    errorFieldCssClass: 'booking__input_invalid',
-    successFieldCssClass: 'booking__input_valid ',
-  });
 
   const bookingInputName = document.querySelector('.booking__input_fullname');
   const bookingInputPhone = document.querySelector('.booking__input_phone');
   const bookingInputTIcket = document.querySelector('.booking__input_ticket');
 
-  new Inputmask('+375(99)999-99-99').mask(bookingInputPhone);
-  new Inputmask('9999').mask(bookingInputTIcket);
+  const comedians = await getComedians();
 
-  validate
-    .addField(bookingInputName, [{
-      rule: 'required',
-      errorMessage: 'Заполните имя',
-    }
-    ])
-    .addField(bookingInputPhone, [{
-      rule: 'required',
-      errorMessage: 'Заполните телефон',
-    }, {
-      validator() {
-        const phone = bookingInputPhone.inputmask.unmaskedvalue();
-        return phone.length === 9;
-      },
-      errorMessage: 'Некорректный телефон',
-    }
-    ])
-    .addField(bookingInputTIcket, [{
-      rule: 'required',
-      errorMessage: 'Заполните номер билета',
-    }, {
-      validator() {
-        const ticket = bookingInputTIcket.inputmask.unmaskedvalue();
-        return ticket.length === 4;
-      },
-      errorMessage: 'Неверный номер билета',
-    }
-    ]).onFail((fields) => {
-      let errorMessage = '';
+  initForm(bookingForm, bookingInputName, bookingInputPhone, bookingInputTIcket);
 
-      for (const key in fields) {
-        if (!Object.hasOwnProperty.call(fields, key)) {
-          continue;
-        }
-        const element = fields[key];
-        if(!element.isValid) {
-          errorMessage += `${element.errorMessage}, `;
-        }
-      }
-
-      notification.show(errorMessage.slice(0, -2), false);
-    });
-
-  bookingForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const data = {
-      booking: [],
-    };
-    const times = new Set();
-
-    new FormData(bookingForm).forEach((value, field) => {
-      if (field === 'booking') {
-        const [comedian, time] = value.split(',');
-
-        if (comedian && time) {
-          data.booking.push({
-            comedian,
-            time,
-          });
-          times.add(time);
-        }
-
-      } else {
-        data[field] = value;
-      }
-
-      if (times.size !== data.booking.length) {
-        notification.show('Нельзя быть в одно время на двух выступлениях', false);
-      }
-
-    });
-  });
+  if (comedians) {
+    countComedians.textContent = comedians.length;
+    const comedianBlock = createComedianBlock(comedians, bookingComedianList);
+    bookingComedianList.append(comedianBlock);
+  }
 };
 
 init();
---
+++ scripts/form.js
@@ -0,0 +1,90 @@
+import Inputmask from 'inputmask';
+import JustValidate from 'just-validate';
+import { Notification } from './notification.js';
+
+export const initForm = (bookingForm, bookingInputName, bookingInputPhone, bookingInputTIcket) => {
+
+  const notification = Notification.getInstance();
+
+  const validate = new JustValidate(bookingForm, {
+    errorFieldCssClass: 'booking__input_invalid',
+    successFieldCssClass: 'booking__input_valid ',
+  });
+
+  new Inputmask('+375(99)999-99-99').mask(bookingInputPhone);
+  new Inputmask('9999').mask(bookingInputTIcket);
+
+  validate
+  .addField(bookingInputName, [{
+    rule: 'required',
+    errorMessage: 'Заполните имя',
+  }
+  ])
+  .addField(bookingInputPhone, [{
+    rule: 'required',
+    errorMessage: 'Заполните телефон',
+  }, {
+    validator() {
+      const phone = bookingInputPhone.inputmask.unmaskedvalue();
+      return phone.length === 9;
+    },
+    errorMessage: 'Некорректный телефон',
+  }
+  ])
+  .addField(bookingInputTIcket, [{
+    rule: 'required',
+    errorMessage: 'Заполните номер билета',
+  }, {
+    validator() {
+      const ticket = bookingInputTIcket.inputmask.unmaskedvalue();
+      return ticket.length === 4;
+    },
+    errorMessage: 'Неверный номер билета',
+  }
+  ]).onFail((fields) => {
+    let errorMessage = '';
+
+    for (const key in fields) {
+      if (!Object.hasOwnProperty.call(fields, key)) {
+        continue;
+      }
+      const element = fields[key];
+      if(!element.isValid) {
+        errorMessage += `${element.errorMessage}, `;
+      }
+    }
+
+    notification.show(errorMessage.slice(0, -2), false);
+  });
+
+bookingForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const data = {
+    booking: [],
+  };
+  const times = new Set();
+
+  new FormData(bookingForm).forEach((value, field) => {
+    if (field === 'booking') {
+      const [comedian, time] = value.split(',');
+
+      if (comedian && time) {
+        data.booking.push({
+          comedian,
+          time,
+        });
+        times.add(time);
+      }
+
+    } else {
+      data[field] = value;
+    }
+
+    if (times.size !== data.booking.length) {
+      notification.show('Нельзя быть в одно время на двух выступлениях', false);
+    }
+
+  });
+});
+}
