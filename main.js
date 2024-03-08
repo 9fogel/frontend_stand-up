dffb18a8ddfee01803f87c01b9e821d3337373e1
Vera Kirayonak <Vera_Kirayonak@epam.com>
2024-03-08 19:14:24 +0300

    feat: add sections switch

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css';
 import { initForm } from './scripts/form';
 import { getComedians } from './scripts/api';
 import { createComedianBlock } from './scripts/comedians';
+import { initChangeSection } from './scripts/changeSection';
 
 const init = async () => {
   const bookingComedianList = document.querySelector('.booking__comedians-list');
@@ -13,14 +14,22 @@ const init = async () => {
   const bookingInputPhone = document.querySelector('.booking__input_phone');
   const bookingInputTIcket = document.querySelector('.booking__input_ticket');
 
-  const comedians = await getComedians();
+  const event = document.querySelector('.event');
+  const booking = document.querySelector('.booking');
+  const eventButtonReserve = document.querySelector('.event__button_reserve');
+  const eventButtonEdit = document.querySelector('.event__button_edit');
+  const bookingTitle = document.querySelector('.booking__title');
 
   initForm(bookingForm, bookingInputName, bookingInputPhone, bookingInputTIcket);
+
+  const comedians = await getComedians();
 
   if (comedians) {
     countComedians.textContent = comedians.length;
     const comedianBlock = createComedianBlock(comedians, bookingComedianList);
     bookingComedianList.append(comedianBlock);
+
+    initChangeSection(event, booking, eventButtonReserve, eventButtonEdit, bookingTitle, bookingForm);
   }
 };
 
